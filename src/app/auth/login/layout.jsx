86da4b10d364c871ebd8fbd43f972fdb664a17ae
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-06-20 14:22:25 +0600

    Minor change

--- src/app/auth/login/layout.jsx
+++ src/app/auth/login/layout.jsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "Login | MediQueue",
-  description: "Access your HealZen patient dashboard to manage appointments, messages, and digital medical history.",
+  description: "Access your MediQueue patient dashboard to manage appointments, messages, and digital medical history.",
 };
 
 export default function LoginLayout({ children }) {
